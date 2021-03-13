dd3276d5f4b8f9784d601e02a41c95c1b5030548
thitran278 <cntthue@CNTTs-MacBook-Air.local>
2021-03-13 10:42:07 +0700

    Up appbarr

--- tabbar_example/lib/Homepage.dart
+++ tabbar_example/lib/Homepage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+final GlobalKey<ScaffoldState> scafoldKey = GlobalKey<ScaffoldState>();
+final SnackBar snackBar = const SnackBar(content: Text("Notification Clicked"));
+
 class Homepage extends StatefulWidget {
   Homepage({Key key}) : super(key: key);
 
@@ -11,7 +14,24 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(child: Text("Create tabbar with Flutter")),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add_alert),
+              tooltip: "Show snackbar",
+              onPressed: () {
+                print("Showed");
+              }),
+          IconButton(
+              icon: Icon(Icons.close),
+              tooltip: 'Close',
+              onPressed: () {
+                print("Closed");
+              })
+        ],
+        title: Text("Create appbar with flutter"),
+      ),
+      body: Container(child: Text("Create tabbar with Flutter")),
     );
   }
 }
